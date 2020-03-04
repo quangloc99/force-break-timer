bdbb5b0a846de5dd3ba2fabba69b9f43a16c4bec
Tran Quang Loc <quangloctran99@gmail.com>
2020-03-04 12:44:51 +0300

    Add TimePickerWidget

---
+++ TimePickerWidget.py
@@ -0,0 +1,67 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, GObject
+from typing import Tuple, Union, Any, Dict
+from datetime import datetime
+
+class TimePickerWidget(Gtk.HBox):
+    __gsignals__: Dict[str, Tuple[Any, Any, Any]] = {
+            "changed": (GObject.SignalFlags.RUN_LAST, None, tuple()),
+    }
+
+    def __init__(self, hours=0, minutes=0, **kwargs):
+        super().__init__(**kwargs)
+
+        self._hours_input = Gtk.SpinButton.new_with_range(0, 23, 1)
+        self._hours_input.set_value(hours)
+        self._hours_input.set_wrap(True)
+
+        self._minutes_input = Gtk.SpinButton.new_with_range(0, 59, 5)
+        self._minutes_input.set_value(minutes)
+        self._minutes_input.set_wrap(True)
+
+        self._init_layout()
+        self._connect_signals()
+
+    def _init_layout(self):
+        self.pack_start(self._hours_input, False, False, 0)
+        self.pack_start(Gtk.Label(label=" h  "), False, True, 0)
+        self.pack_start(self._minutes_input, False, False, 0)
+        self.pack_start(Gtk.Label(label=" m  "), False, True, 0)
+
+    def _connect_signals(self):
+        try:
+            if self._connected_signals:
+                return
+        except AttributeError:
+            self._hours_input.connect('value-changed', self._value_changed_callback)
+            self._minutes_input.connect('value-changed', self._value_changed_callback)
+            self._connected_signals = True
+
+    def _value_changed_callback(self, sender):
+        self.emit('changed')
+        
+    def get_hours(self) -> int:
+        return int(self._hours_input.get_value())
+
+    def get_minutes(self) -> int:
+        return int(self._minutes_input.get_value())
+    
+    def set_hours(self, value):
+        self._hours_input.set_value(value)
+
+    def set_minutes(self, value):
+        self._minutes_input.set_value(value)
+
+    def set_hours_and_minutes(self, hours: int, minutes: int):
+        self._hours_input.set_value(hours)
+        self._minutes_input.set_value(minutes)
+
+    def get_as_datetime(self, now = datetime.now()):
+        return now.replace(hour = self.get_hours(), minutes = self.get_minutes())
+
+    def get_as_timedelta(self):
+        return timedelta(hour = self.get_hours(), minutes = self.get_minutes()) 
+
+    def get_hours_and_minutes(self) -> Tuple[int, int]:
+        return self.get_hours(), self.get_minutes()
