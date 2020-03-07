5cd6cab042ff31586b353478cff18faff6b62195
Tran Quang Loc <quangloctran99@gmail.com>
2020-03-07 18:02:30 +0300

    Modify Timeline's to handle the line

--- Timeline.py
+++ Timeline.py
@@ -2,13 +2,16 @@ import gi
 from gi.repository import GLib, GObject
 from typing import *
 
+TimelineList = Iterable[Union[int, Callable[..., bool]]]
+
 class Timeline(GObject.Object):
     __gsignals__: Dict[str, Tuple[Any, Any, Any]] = {
             "stopped": (GObject.SignalFlags.RUN_FIRST, None, ())
     }
-    def __init__(self, timeline: List[Tuple[int, Callable[[], bool]]]):
+    def __init__(self, timeline: TimelineList):
         super().__init__()
-        self.__timeline_iter = iter(timeline)
+        self.__timeline = list(timeline)
+        self.__iter = self.__timeline_iter()
         self.__started = False
         self.__stopped = False
         self.__cur_elm = None
@@ -18,7 +21,7 @@ class Timeline(GObject.Object):
         if self.__started:
             return 
         self.__started = True
-        self.__set_timeout_next()
+        self.__run_next()
 
     def stop(self):
         if self.__stopped:
@@ -29,24 +32,28 @@ class Timeline(GObject.Object):
         self.__stopped = True
         self.emit("stopped")
 
-    def __set_timeout_next(self):
-        try:
-            self.__cur_elm = next(self.__timeline_iter)
-            duration = self.__cur_elm[0]
-            self.__timeout_id = GLib.timeout_add(duration, self.__timeout_callback)
-        except StopIteration:
-            self.stop()
+    def __timeline_iter(self):
+        for i in self.__timeline:
+            if self.__stopped:
+                return 
+            if type(i) == int:
+                self.__timeout_id = GLib.timeout_add(i, self.__run_next)
+                yield
+            else:
+                res = i()
+                if res is not None and not res:
+                    self.stop()
+                    break
 
-    def __timeout_callback(self):
+    def __run_next(self):
         self.__timeout_id = -1
         if self.__stopped:
-            return 
-        cb = self.__cur_elm[1]
-        ret = cb()
-        if ret is not None and not ret:
-            self.stop()
             return False
-        self.__set_timeout_next()
-        return False
+        try:
+            next(self.__iter)
+            return False
+        except StopIteration:
+            return False
+
 
 
